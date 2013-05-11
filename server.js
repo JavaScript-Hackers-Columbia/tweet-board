fea22c2746ef62853891fa24595ac0015c42fdb3
alberte <dropped.on.japan@gmail.com>
2013-05-11 16:51:24 -0500

    server.js: Added HTTPS implementation for Twitter oAuth.

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express'),
-    app = module.exports = express();
+    app = module.exports = express(),
+    uuid = require('node-uuid'),
+    request = require('request'),
+    https = require('https'),
+    http = require('http'),
+    fs = require('fs');
+
+var oauthToken = '14744138-NTKGRb2JlbKOIrVTwBXY6JwbTWXyJgFS1RFaJzQXP';
+var options = {
+  key : fs.readFileSync(__dirname + '/../key.pem'),
+  cert : fs.readFileSync(__dirname + '/../cert.pem')
+};
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', {
+    'layout' : true
+  });
 
   app.use(express.cookieParser('my secret here'));
   app.use(express.bodyParser());
@@ -15,10 +29,16 @@ app.configure(function(){
 });
 
 app.get('/', function(req, res){
-  res.render('index', {});
+  res.render('index', { token : oauthToken });
 });
 
+app.get('/twitter/oauth', function() {
+  console.log(req.body);
+  res.end('<h1>What whaat?!</h1>');
+});
 
-app.listen('3000');
+https.createServer(options, app).listen('3200');
+http.createServer(app).listen('3000');
 
-console.log( 'Express started on port %d', 3000 );
+console.log( 'HTTP -  Express started on port %d', 3000 );
+console.log( 'HTTPS - Express started on port %d', 3200 );
